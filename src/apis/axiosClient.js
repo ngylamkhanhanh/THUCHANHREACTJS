6806a3e9b7ce94f074afb9faa046c0adf7a7f2d8
Khanh Anh <ngylamkhanhanh@gmail.com>
2026-04-19 21:15:57 +0700

    fix api path

--- src/apis/axiosClient.js
+++ src/apis/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosClient = axios.create({
-    baseURL: 'http://localhost:3000/api/v1',
+    baseURL: 'https://thuchanhreactjs-4ovg.vercel.app/api/v1',
     timeout: 1000,
     headers: {
         'Content-Type': 'application/json',
